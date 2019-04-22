8fbe6e14ff46375a894f3287371452bace61db8a
echterna <echterna@usc.edu>
2019-04-22 15:33:07 -0700

    working gui

--- gui.c
+++ gui.c
@@ -26,23 +26,42 @@ void gui_display_home(void) {
     LCD_WHITE, 1);
 
   // Heart Button
-  uint16_t heart_center_x = lcd_width() / 2 + 50;
-  uint16_t heart_center_y = lcd_height() / 2 + 20;
-  fill_circle(heart_center_x, heart_center_y, BTN_RADIUS, LCD_DARKGREY);
-  draw_heart(
-    heart_center_x - 16,
-    heart_center_y - 16,
-    LCD_WHITE, 1);
+	uint16_t heart_center_x = lcd_width() / 2 + 50;
+	uint16_t heart_center_y = lcd_height() / 2 + 20;
+	fill_circle(heart_center_x, heart_center_y, BTN_RADIUS, LCD_DARKGREY);
+	draw_heart(
+	heart_center_x - 16,
+	heart_center_y - 16,
+	LCD_WHITE, 1);
+
+	// Compass Button 
+	uint16_t compass_center_x = lcd_width() / 2 ;
+	uint16_t compass_center_y= lcd_height() / 2 + 80;
+	fill_circle(compass_center_x, compass_center_y, BTN_RADIUS, LCD_DARKGREY);
+	draw_text(compass_center_x - text_width(3) / 2 - 5, compass_center_y - text_width(3) / 2 + 15, "COMP",	LCD_WHITE, 1);
 }
 
 void gui_display_SOS(void) {
-  fill(LCD_BLACK);
+  fill(LCD_RED);
 
-  draw_text(80, 90, "SOS", LCD_RED, 4);
+  draw_text(85, 80, "SOS", LCD_BLACK, 3);
+  draw_text(70, 130, "SENT", LCD_BLACK, 3);
 }
 
 void gui_display_GPS(void){
-	fill(LCD_RED);
+	fill(LCD_DARKGREY);
 	get_data();
 	draw_text(130, 20, "GPS", LCD_WHITE, 2);
+}
+
+void gui_display_COMPASS(void)
+{
+	fill(LCD_DARKGREY);
+	draw_text(70,30, "COMPASS", LCD_WHITE, 2);
+}
+
+void gui_display_PULSE(void)
+{
+	fill(LCD_BLACK);
+	draw_text(90,30, "PULSE", LCD_DARKGREY, 2);
 }
